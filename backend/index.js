0293a65fc452668d9d50e77ea150cb45da8e56aa
Dhruvi Shah <ddshah14124@gmail.com>
2025-07-21 20:56:40 +0530

    Added dynamic port for Render deployment

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000;
 const mongoose = require('mongoose')
 const cors = require('cors')
 const authRoutes = require('./routes/auth')
